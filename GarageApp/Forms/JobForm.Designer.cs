96f5f08607aee72e7b987a2f76520d67c12e0c9d
Nathan Stork <nejc.stork@hotmail.nl>
2022-11-27 01:23:55 +0100

    (Add) Job form

---
+++ GarageApp/Forms/JobForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GarageApp.Forms
+{
+    partial class JobForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JobForm));
+            this.submitButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.priceNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.priceLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.submitButton.Location = new System.Drawing.Point(129, 547);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(224, 40);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(28, 212);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 125);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Car";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(28, 367);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(426, 125);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Customer";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(28, 25);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(88, 20);
+            this.descriptionLabel.TabIndex = 3;
+            this.descriptionLabel.Text = "Description:";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(28, 48);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.descriptionTextBox.Size = new System.Drawing.Size(426, 125);
+            this.descriptionTextBox.TabIndex = 4;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(207, 181);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(44, 20);
+            this.dateLabel.TabIndex = 5;
+            this.dateLabel.Text = "Date:";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(257, 179);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(197, 27);
+            this.dateTimePicker.TabIndex = 6;
+            this.dateTimePicker.Value = new System.DateTime(2022, 11, 26, 23, 39, 43, 0);
+            // 
+            // priceNumericUpDown
+            // 
+            this.priceNumericUpDown.Location = new System.Drawing.Point(78, 179);
+            this.priceNumericUpDown.Name = "priceNumericUpDown";
+            this.priceNumericUpDown.Size = new System.Drawing.Size(115, 27);
+            this.priceNumericUpDown.TabIndex = 7;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(28, 181);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(44, 20);
+            this.priceLabel.TabIndex = 8;
+            this.priceLabel.Text = "Price:";
+            // 
+            // JobForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(482, 615);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.priceNumericUpDown);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.submitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JobForm";
+            this.Padding = new System.Windows.Forms.Padding(25);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Register a new job";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.priceNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button submitButton;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label descriptionLabel;
+        private TextBox descriptionTextBox;
+        private Label dateLabel;
+        private DateTimePicker dateTimePicker;
+        private NumericUpDown priceNumericUpDown;
+        private Label priceLabel;
+    }
+}
---
+++ GarageApp/Forms/JobForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarageApp.Forms
+{
+    public partial class JobForm : Form
+    {
+        private Action Callback;
+
+        public JobForm(Action? callback = null)
+        {
+            Callback = callback;
+
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            Employees employees = Employees.GetInstance();
+            employees.CurrentUser.Garage.Jobs.Add(new Job(
+                descriptionTextBox.Text,
+                dateTimePicker.Value.ToString(),
+                Convert.ToInt32(priceNumericUpDown.Value),
+                new Car("", "", "", CarColor.Grey),
+                new Customer("", "", "", "")
+            ));
+
+            if (Callback != null)
+            {
+                Callback();
+            }
+
+            Console.WriteLine(employees.CurrentUser.Garage.Jobs.Count);
+
+            this.Hide();
+            this.Close();
+        }
+    }
+}
