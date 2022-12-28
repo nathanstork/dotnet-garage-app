9d832870b75ad51aaeae5925a22c04c33a713172
Nathan Stork <nejc.stork@hotmail.nl>
2022-12-28 18:00:13 +0100

    Removed unnecessary employees unit tests and code

--- GarageAppTests/EmployeesTests.cs
+++ GarageAppTests/EmployeesTests.cs
@@ -1,8 +1,4 @@
-using GarageApp.Contracts;
-using GarageApp.Users;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace GarageAppTests
 {
@@ -13,14 +9,11 @@ namespace GarageAppTests
     public class EmployeesTests
     {
         private Employees Employees;
-        private string SaveFilePath;
 
         [SetUp]
         public void Setup()
         {
             Employees = Employees.GetInstance();
-
-            SaveFilePath = Employees.SaveFilePath;
 
             Trace.Listeners.Add(new ConsoleTraceListener());
         }
@@ -36,6 +29,8 @@ namespace GarageAppTests
             {
                 Assert.Fail(ex.Message);
             }
+
+            Employees.LogOut(); // Clean up by logging out
         }
 
         [Test]
@@ -59,46 +54,6 @@ namespace GarageAppTests
             Employees.LogOut();
 
             if (Employees.CurrentUser != null) Assert.Fail();
-        }
-
-        [Test]
-        public void SaveDataTest()
-        {
-            string randomUsername = Guid.NewGuid().ToString();
-            string randomPassword = Guid.NewGuid().ToString();
-            string randomName = Guid.NewGuid().ToString();
-            string randomAddress = Guid.NewGuid().ToString();
-
-            GarageApp.Users.Mechanic newMechanic = new GarageApp.Users.Mechanic(
-                randomUsername, randomPassword, randomName, randomAddress, new MonthlyContract(1, 1)
-            );
-            Employees.Mechanics.Add(newMechanic);
-
-            GarageApp.Users.Manager newManager = new GarageApp.Users.Manager(
-                randomUsername, randomPassword, randomName, randomAddress, new MonthlyContract(1, 1), Employees.Mechanics
-            );
-            Employees.Managers.Add(newManager);
-
-            Employees.SaveData();
-
-            Stream stream = File.Open(SaveFilePath, FileMode.Open);
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-
-            ValueTuple<List<Mechanic>, List<Manager>> data = (ValueTuple<List<Mechanic>, List<Manager>>) binaryFormatter.Deserialize(stream);
-
-            if (!data.Item1.Contains(newMechanic) || !data.Item2.Contains(newManager))
-            {
-                Assert.Fail();
-            }
-
-            // Serialize current managers and mechanics
-            // Deserialize save file 
-            // Compare resulting strings
-
-            // Revert changes
-            Employees.Mechanics.Remove(newMechanic);
-            Employees.Managers.Remove(newManager);
-            Employees.SaveData();
         }
     }
 }
